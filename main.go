96c4081f4d8443368ccf2eaf7930a686d5ba0bcd
Jeremiah Orem <oremj@oremj.com>
2016-09-20 08:58:30 -0700

    main: add -debug flag

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/defaults"
 	"github.com/aws/aws-sdk-go/aws/signer/v4"
 	"github.com/oremj/aws-elasticsearch-proxy/proxy"
@@ -15,6 +16,7 @@ import (
 var addr = flag.String("addr", "127.0.0.1:8080", "Listen address")
 var esEndpoint = flag.String("es-endpoint", "", "ElasticSearch endpoint e.g., my-es.us-east-1.es.amazonaws.com")
 var region = flag.String("region", "us-east-1", "AWS region")
+var debug = flag.Bool("debug", false, "Enables additional logging")
 
 func main() {
 	flag.Parse()
@@ -24,6 +26,12 @@ func main() {
 		os.Exit(2)
 	}
 	signer := v4.NewSigner(defaults.Get().Config.Credentials)
+
+	if *debug {
+		signer.Debug = aws.LogDebugWithSigning
+		signer.Logger = aws.NewDefaultLogger()
+	}
+
 	es := proxy.NewElasticSearch(*esEndpoint, *region, signer)
 	log.Fatal(http.ListenAndServe(*addr, es))
 }
